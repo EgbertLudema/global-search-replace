6badd1bececa0d57098fe62f62bf18bda778d47a
Egbert Ludema <egbertludema2001@gmail.com>
2025-02-26 15:00:48 +0100

    Updates:
    - Added select table function + apply selected button
    - Updated styling:
        - Made the Table titles + checkboxes sticky when viewing that table
        - Spacing
        - General styling

--- includes/search-replace.php
+++ includes/search-replace.php
@@ -24,70 +24,80 @@ function gsr_preview_results($search, $replace, $use_regex, $selected_tables) {
             }
 
             foreach ($selected_tables as $table) {
-                echo "<h3>Table: " . esc_html($table) . "</h3>";
+                echo '<div class="table-container">';
+                    echo "<div class='table-header'>";
+                        echo "<input type='checkbox' name='selected_tables[]' value='" . esc_attr($table) . "'> ";
+                        echo "<h3>Table: " . esc_html($table) . "</h3>";
+                    echo "</div>";
 
-                $columns = $wpdb->get_col("SHOW COLUMNS FROM `$table`");
+                    $columns = $wpdb->get_col("SHOW COLUMNS FROM `$table`");
 
-                foreach ($columns as $column) {
-                    $sql = "SELECT * FROM `$table` WHERE `$column` LIKE %s";
-                    $results = $wpdb->get_results($wpdb->prepare($sql, '%' . $wpdb->esc_like($search) . '%'));
+                    foreach ($columns as $column) {
+                        $sql = "SELECT * FROM `$table` WHERE `$column` LIKE %s";
+                        $results = $wpdb->get_results($wpdb->prepare($sql, '%' . $wpdb->esc_like($search) . '%'));
 
-                    if ($results) {
-                        // Add select button
-                        echo "<h4>Column: {$column}</h4><ul class='before-after-list'>";
-                        foreach ($results as $row) {
-                            $old_value = $row->$column ?? '';
-                            if ($old_value === '') {
-                                continue;
-                            }
+                        if ($results) {
+                            echo "<h4>Column: {$column}</h4>";
+                            echo "<ul class='before-after-list'>";
+                                foreach ($results as $row) {
+                                    $old_value = $row->$column ?? '';
+                                    if ($old_value === '') {
+                                        continue;
+                                    }
 
-                            $search_highlighted = "<span class='highlighted'>{$search}</span>";
-                            $replace_highlighted = "<span class='highlighted'>{$replace}</span>";
+                                    $search_highlighted = "<span class='highlighted'>{$search}</span>";
+                                    $replace_highlighted = "<span class='highlighted'>{$replace}</span>";
 
-                            if ($use_regex) {
-                                $new_value = preg_replace("/{$search}/i", $replace, $old_value);
-                                $old_value_highlighted = preg_replace("/{$search}/i", $search_highlighted, $old_value);
-                                $new_value_highlighted = preg_replace("/{$replace}/i", $replace_highlighted, $new_value);
-                            } else {
-                                $new_value = str_ireplace($search, $replace, $old_value);
-                                $old_value_highlighted = str_ireplace($search, $search_highlighted, $old_value);
-                                $new_value_highlighted = str_ireplace($replace, $replace_highlighted, $new_value);
-                            }
+                                    if ($use_regex) {
+                                        $new_value = preg_replace("/{$search}/i", $replace, $old_value);
+                                        $old_value_highlighted = preg_replace("/{$search}/i", $search_highlighted, $old_value);
+                                        $new_value_highlighted = preg_replace("/{$replace}/i", $replace_highlighted, $new_value);
+                                    } else {
+                                        $new_value = str_ireplace($search, $replace, $old_value);
+                                        $old_value_highlighted = str_ireplace($search, $search_highlighted, $old_value);
+                                        $new_value_highlighted = str_ireplace($replace, $replace_highlighted, $new_value);
+                                    }
 
-                            $allowed_tags = [
-                                'span' => ['class' => []],
-                                'strong' => [],
-                                'em' => [],
-                                'b' => [],
-                                'i' => [],
-                                'p' => [],
-                                'br' => [],
-                                'ul' => [],
-                                'ol' => [],
-                                'li' => [],
-                                'a' => ['href' => [], 'title' => []]
-                            ];
+                                    $allowed_tags = [
+                                        'span' => ['class' => []],
+                                        'strong' => [],
+                                        'em' => [],
+                                        'b' => [],
+                                        'i' => [],
+                                        'p' => [],
+                                        'br' => [],
+                                        'ul' => [],
+                                        'ol' => [],
+                                        'li' => [],
+                                        'a' => ['href' => [], 'title' => []]
+                                    ];
 
-                            echo "<li>";
-                                echo "<div class='before'><strong>Before:</strong> " . wp_kses($old_value_highlighted, $allowed_tags) . "</div>";
-                                echo "<div class='after'><strong>After:</strong> " . wp_kses($new_value_highlighted, $allowed_tags) . "</div>";
-                            echo "</li>";
+                                    echo "<li>";
+                                        echo "<div class='before'><strong>Before:</strong> " . wp_kses($old_value_highlighted, $allowed_tags) . "</div>";
+                                        echo "<div class='after'><strong>After:</strong> " . wp_kses($new_value_highlighted, $allowed_tags) . "</div>";
+                                    echo "</li>";
 
+                                }
+                            echo "</ul>";
                         }
-                        echo "</ul>";
                     }
-                }
+                echo "</div>";
             }
 
-            // Apply Changes button
-            echo '<input type="submit" name="gsr_apply" value="Apply Changes" class="button-primary">';
-            // Add apply selected changes button
+            // Apply Changes buttons
+            echo '<div class="apply_buttons">';
+                echo '<input type="submit" name="gsr_apply" value="Apply all Changes" class="button-secondary">';
+                echo '<input type="submit" name="gsr_apply_selected" value="Apply selected Tables" class="button-primary">';
+            echo '</div>';
         echo '</form>';
     echo "</div>";
 
     // Process form submission (Check if Apply Changes was clicked)
     if (isset($_POST['gsr_apply'])) {
-        // error_log("DEBUG: Apply button clicked. Running gsr_apply_changes()."); // Check if function is called
         gsr_apply_changes($_POST['search_text'], $_POST['replace_text'], isset($_POST['use_regex']), $_POST['tables']);
+    } else if (isset($_POST['gsr_apply_selected'])) {
+        if (!empty($_POST['selected_tables'])) {
+            gsr_apply_changes($_POST['search_text'], $_POST['replace_text'], isset($_POST['use_regex']), $_POST['selected_tables']);
+        }
     }
 }
