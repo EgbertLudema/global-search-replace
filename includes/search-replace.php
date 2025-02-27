84c966a13bf79647aecec23cc91846fcd4f9709e
Egbert Ludema <egbertludema2001@gmail.com>
2025-02-27 11:30:26 +0100

    - Added permalink to posts - Display post_type - Added context - Updated some styling - Added target blank svg to link

--- includes/search-replace.php
+++ includes/search-replace.php
@@ -59,7 +59,11 @@ function gsr_preview_results($search, $replace, $use_regex, $selected_tables) {
                                     }
 
                                     $allowed_tags = [
-                                        'span' => ['class' => []],
+                                        'span' => ['class' => []], // Span with class highlighted
+                                        'h1' => [],
+                                        'h2' => [],
+                                        'h3' => [],
+                                        'h4' => [],
                                         'strong' => [],
                                         'em' => [],
                                         'b' => [],
@@ -72,7 +76,30 @@ function gsr_preview_results($search, $replace, $use_regex, $selected_tables) {
                                         'a' => ['href' => [], 'title' => []]
                                     ];
 
+                                    // var_dump($row);
+
                                     echo "<li>";
+                                        if ($table === 'wp_postmeta' or $table === 'wp_posts') {
+                                            $post_id = isset($row->post_id) ? $row->post_id : (isset($row->ID) ? $row->ID : null);
+                                            $post_type = null;
+
+                                            if ($post_id) {
+                                                $post_type = $wpdb->get_var($wpdb->prepare("SELECT post_type FROM wp_posts WHERE ID = %d", $post_id));
+                                            }
+
+
+                                            echo "<div class='post-info'>";
+                                            if ($post_type != null){
+                                                echo "<p><strong>Post type:</strong> " . $post_type . "</p>";
+                                            }
+
+                                            if ($post_id && $post_type === 'post' or $post_type === 'page') {
+                                                echo "<a target='_blank' href='" . get_permalink($post_id) . "'>Link to post";
+                                                    echo '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_iconCarrier"> <g id="Interface / External_Link"> <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>';
+                                                echo "</a>";
+                                            }
+                                            echo "</div>";
+                                        }
                                         echo "<div class='before'><strong>Before:</strong> " . wp_kses($old_value_highlighted, $allowed_tags) . "</div>";
                                         echo "<div class='after'><strong>After:</strong> " . wp_kses($new_value_highlighted, $allowed_tags) . "</div>";
                                     echo "</li>";
